dfa4532707b29768a2f1b6db92d7dfcbb7bfa85c
vicocotea <laplace.victor@gmail.com>
2025-07-15 15:26:48 +0200

    asset path

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: 'export',
+  basePath: isProd ? '/svg-shaders' : '',
+  assetPrefix: isProd ? '/svg-shaders/' : '',
   trailingSlash: true,
   images: {
     unoptimized: true
